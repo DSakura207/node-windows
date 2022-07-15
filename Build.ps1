abf6908029a85111aaa692bc89149066912faf55
DSakura207 <4421906+DSakura207@users.noreply.github.com>
2022-07-14 22:58:00 -0600

    Switch to real build

--- Build.ps1
+++ Build.ps1
@@ -59,10 +59,8 @@ $cmdline = "docker build `
             --build-arg BASE_IMAGE_NAME=$env:BASE_IMAGE_NAME `
             --build-arg BASE_IMAGE_TAG=$env:BASE_IMAGE_TAG `
             --build-arg NODE_VERSION=$node_full_ver `
-            -t node-windows:$env:NODE_VERSION `
-            -t node-windows:$node_major_ver `
-            -t node-windows:$node_full_ver ." 
+            -t node-windows:$env:NODE_VERSION-$env:BASE_IMAGE_TAG `
+            -t node-windows:$node_major_ver-$env:BASE_IMAGE_TAG `
+            -t node-windows:$node_full_ver-$env:BASE_IMAGE_TAG ." 
 
-Write-Host "Build commandline:$cmdline" 
-
-#& $cmdline
+& $cmdline

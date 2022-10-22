fc951e06eb9bb028304faff2791fd82afd9d9d3b
DSakura207 <4421906+DSakura207@users.noreply.github.com>
2022-10-22 17:52:11 -0600

    Fix null for no matched tag

--- Build.ps1
+++ Build.ps1
@@ -66,22 +66,29 @@ foreach ($item in $releaseArray) {
 }
 
 $node_major_ver = $buildList[$env:NODE_VERSION]
-$node_full_ver = $versionTags[$node_major_ver]
 
-Write-Debug "===Build info==="
-Write-Debug "Version tag: $env:NODE_VERSION"
-Write-Debug "Major version: $node_major_ver"
-Write-Debug "Full version: $node_full_ver"
+# Sometimes a version tag cannot be matched to a version.
+# Wrap build commands in an if block to prevent a null error.
+if ($node_major_ver) {
+    $node_full_ver = $versionTags[$node_major_ver]
 
-$buildArg = @(
-    "build"
-    "--build-arg BASE_IMAGE_NAME=$env:BASE_IMAGE_NAME"
-    "--build-arg BASE_IMAGE_TAG=$env:BASE_IMAGE_TAG"
-    "--build-arg NODE_VERSION=$node_full_ver"
-    "-t $env:DOCKER_HUB_USERNAME/$($env:REPO_NAME):$env:NODE_VERSION-$env:BASE_IMAGE_TAG"
-    "-t $env:DOCKER_HUB_USERNAME/$($env:REPO_NAME):$node_major_ver-$env:BASE_IMAGE_TAG"
-    "-t $env:DOCKER_HUB_USERNAME/$($env:REPO_NAME):$node_full_ver-$env:BASE_IMAGE_TAG"
-    "."
-)
+    Write-Debug "===Build info==="
+    Write-Debug "Version tag: $env:NODE_VERSION"
+    Write-Debug "Major version: $node_major_ver"
+    Write-Debug "Full version: $node_full_ver"
 
-Start-Process -FilePath docker.exe -ArgumentList $buildArg -Wait -NoNewWindow -ErrorAction Stop
+    $buildArg = @(
+        "build"
+        "--build-arg BASE_IMAGE_NAME=$env:BASE_IMAGE_NAME"
+        "--build-arg BASE_IMAGE_TAG=$env:BASE_IMAGE_TAG"
+        "--build-arg NODE_VERSION=$node_full_ver"
+        "-t $env:DOCKER_HUB_USERNAME/$($env:REPO_NAME):$env:NODE_VERSION-$env:BASE_IMAGE_TAG"
+        "-t $env:DOCKER_HUB_USERNAME/$($env:REPO_NAME):$node_major_ver-$env:BASE_IMAGE_TAG"
+        "-t $env:DOCKER_HUB_USERNAME/$($env:REPO_NAME):$node_full_ver-$env:BASE_IMAGE_TAG"
+        "."
+    )
+
+    Start-Process -FilePath docker.exe -ArgumentList $buildArg -Wait -NoNewWindow -ErrorAction Stop
+}
+
+
